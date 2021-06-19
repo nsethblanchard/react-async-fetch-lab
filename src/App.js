b53b05c814b44c78ff082e3e0ae2a754d3696f00
Seth Blanchard <nsethblanchard@gmail.com>
2021-06-19 09:17:34 -0400

    commit tree 56bd04d5e8a980ea0cab55bfd01fa98339300c7b

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return <div></div>;
+  }
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+}
+
+export default App;
